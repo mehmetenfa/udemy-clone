061c30275bdda902ffe8fcf013b418a434e664c4
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-06-08 23:13:14 +0300

    files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Home } from "./pages";
+import Navbar from "./components/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
-    <div className="App">
-      hello
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
