63ab9ec9fbcfa5c4a5b58d69e6b36fe0eb99f428
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-06-09 23:08:44 +0300

    nav imports

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,9 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import {MdMenu, MdShoppingCart} from "react-icons/md";
+import {Link} from 'react-router-dom';
+import { useSidebarContext } from '../context/sidebar_context';
+import { useCartContext } from '../context/cart_context';
 
 const Navbar = () => {
   return (
