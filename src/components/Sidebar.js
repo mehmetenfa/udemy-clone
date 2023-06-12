471bd5f4ad44cbc4074572ead12c1af88321a0f3
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-06-13 00:03:57 +0300

    .

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,7 @@
-import React from 'react'
+import React from 'react';
+import styled from "styled-components";
+import {MdClose} from "react-icons/md";
+import {Link} from "react-router-dom";
 
 const Sidebar = () => {
   return (
