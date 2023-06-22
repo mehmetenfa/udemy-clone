7fd81ec285de0a1c7682521120ff5bdaa01a39c3
Mehmet Enfa Vakitsayan <106542921+mehmetenfa@users.noreply.github.com>
2023-06-22 23:56:28 +0300

    homepage

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Hero from "../components/Hero";
+import CoursesList from "../components/CourseList";
+import CategoriesList from "../components/CategoriesList";
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div className='holder'>
+      <Hero />
+      <CoursesList />
+      <CategoriesList />
+    </div>
   )
 }
 
